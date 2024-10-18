91a90c6291bc4b3ec614400f0f8d58386d02ec83
donatimar <donatimar@gmail.com>
2024-10-18 15:14:43 +0200

    added comments

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -6,16 +6,17 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
+// esprimo le variabili
 let userWord = prompt("Inserisci la parola");
 let reversedWord = "";
-
 console.log("Parola inserita:", userWord);
 
+// esprimo la funzione
 function checkPalindrome(userWord, reversedWord) {
+  //ciclo for per invertire le parole
   for (let i = userWord.length - 1; i >= 0; i--) {
     reversedWord += userWord[i];
   }
-
   console.log("Parola invertita:", reversedWord);
 
   if (reversedWord === userWord) {
@@ -26,4 +27,6 @@ function checkPalindrome(userWord, reversedWord) {
     alert("La parola non è palindroma");
   }
 }
+
+// risultato finale
 checkPalindrome(userWord, reversedWord);
