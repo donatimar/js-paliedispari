4417ae5c3ecd980029233b73d30451d8ea600477
donatimar <donatimar@gmail.com>
2024-10-18 15:01:22 +0200

    PALINDROMA - added function and ciclo for

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,5 +1,25 @@
+console.log("JS OK");
+
 // ! Palindroma
 /*
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+
+function isPalindrome() {
+  let userWord = prompt("Inserisci la parola");
+  let reversedWord = "";
+
+  function checkPalindrome(userWord, reversedWord) {
+    for (let i = userWord.length - 1; i >= 0; i--) {
+      reversedWord += userWord[i];
+    }
+
+    if (reversedWord === userWord) {
+      alert("La parola è Palindroma");
+    } else {
+      alert("La parola non è Palindroma");
+    }
+  }
+  checkPalindrome(userWord, reversedWord);
+}
