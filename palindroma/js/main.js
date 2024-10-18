432b3a510443df310599378a46e4e54e0267bc63
donatimar <donatimar@gmail.com>
2024-10-18 15:11:53 +0200

    found error

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -6,26 +6,24 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-function isPalindrome() {
-  let userWord = prompt("Inserisci la parola");
-  let reversedWord = "";
+let userWord = prompt("Inserisci la parola");
+let reversedWord = "";
 
-  console.log("Parola inserita:", userWord);
+console.log("Parola inserita:", userWord);
 
-  function checkPalindrome(userWord, reversedWord) {
-    for (let i = userWord.length - 1; i >= 0; i--) {
-      reversedWord += userWord[i];
-    }
-
-    console.log("Parola invertita:", reversedWord);
-
-    if (reversedWord === userWord) {
-      console.log("La parola è palindroma");
-      alert("La parola è palindroma");
-    } else {
-      console.log("La parola non è palindroma");
-      alert("La parola non è palindroma");
-    }
+function checkPalindrome(userWord, reversedWord) {
+  for (let i = userWord.length - 1; i >= 0; i--) {
+    reversedWord += userWord[i];
   }
-  checkPalindrome(userWord, reversedWord);
+
+  console.log("Parola invertita:", reversedWord);
+
+  if (reversedWord === userWord) {
+    console.log("La parola è palindroma");
+    alert("La parola è palindroma");
+  } else {
+    console.log("La parola non è palindroma");
+    alert("La parola non è palindroma");
+  }
 }
+checkPalindrome(userWord, reversedWord);
