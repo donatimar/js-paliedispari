487955df18bfdcba6679c784465db139f4216dd3
donatimar <donatimar@gmail.com>
2024-10-18 15:23:42 +0200

    PARI E DISPARI - added comments

--- pari e dispari/js/main.js
+++ pari e dispari/js/main.js
@@ -8,3 +8,13 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+// funzione per scegliere pari o dispari
+// funzione per generare un numero da 1 a 5
+// esprimo le variabili
+// scegli pari o dispari
+// scegli un numero da 1 a 5
+// genero un numero da 1 a 5 per il computer
+// sommo il numero dell'utente con il numero del computer
+// determino se la somma è pari o dispari
+// dichiaro chi ha vinto
